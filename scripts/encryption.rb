10a9a9b91bbc893211885fd084efb43e6f5d76d5
Lucy Lytle <lucy.lytle@kellogg.northwestern.edu>
2024-05-17 16:15:25 +0000

    classwork

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,13 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret = "puppies"
+encrypted = BCrypt::Password.create(secret)
+puts encrypted
+
 # 2. prepare encrypted string for testing
+entered_password = "puppies"
+outcome = BCrypt::Password.new(encrypted) == entered_password
+
 # 3. test secret against prepared encrypted string
+puts outcome 
